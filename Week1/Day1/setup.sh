47f135bcdda4db4fba9d50e8fb451819f275d396
Snojo <snorri13@ru.is>
2017-11-27 19:26:02 +0000

    small changes to setup.sh

--- Week1/Day1/setup.sh
+++ Week1/Day1/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#set -e
+#set -e doesn't work.... how to log pls
 
 echo "Hello " | $USER
 echo "This is your go-to installation script for everything you want to install!"
@@ -8,7 +8,7 @@ echo "You are currently running on " | uname -a
 read -p "Would you like to install everything, including Visual Studio Code? [Y/n] " response2
 read -p "Are you ready to start? [Y/n] " response
 
-
+#The if statment if you want to continue or not.
 case $response in [yY][eE][sS]|[yY]|[jJ]|'') 
 
     echo
@@ -17,6 +17,7 @@ case $response in [yY][eE][sS]|[yY]|[jJ]|'')
     echo
     #Lets Start!
     echo "LETS GOOOOOO"
+    #This if statement is for optional software.
     case $response2 in [yY][eE][sS]|[yY]|[jJ]|'') 
         sudo add-apt-repository -y ppa:ubuntu-desktop/ubuntu-make
         sudo apt-get -y update
@@ -24,10 +25,12 @@ case $response in [yY][eE][sS]|[yY]|[jJ]|'')
 
         sudo umake ide visual-studio-code
         ;;
-        *)
+        *) #Else
         ;;
     esac
+    ############### Real install starts here ################## 
     sudo apt-get install -y git
+    ############### Ends Here ########################
     #We're Finished!
     echo
     res2=$(date +%s.%N)
@@ -43,7 +46,7 @@ case $response in [yY][eE][sS]|[yY]|[jJ]|'')
 
     printf "Total runtime: %d:%02d:%02d:%02.4f\n" $dd $dh $dm $ds
     ;;
-    *)
+    *) #ELSE
     echo
     echo "Opperation terminated... Goodbye :'( "
     echo
