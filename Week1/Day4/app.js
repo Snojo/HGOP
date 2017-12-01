577d9cc18436afc201ce93feb748555102af0f1d
Snojo <snorri13@ru.is>
2017-12-01 14:28:47 +0000

    Overthinking is a bad thing...

--- Week1/Day4/app.js
+++ Week1/Day4/app.js
@@ -11,60 +11,24 @@ var app = express();
   },
 });*/
 
+// TODO: NEED TO CREATE A CONNECTION STRING.... I THINK
 
 app.get('/', (req, res) => res.status(200).send('hello wooorld!!!'));
 
 
 // Should return an array of 10 item names.
 app.get('/items', (req, res) => {
-  //database.get();
+  database.get();
   // todo
-  const results = [];
-  // Get a Postgres client from the connection pool
-  database.connect(connectionString, (err, client, done) => {
-    // Handle connection errors
-    if(err) {
-      done();
-      console.log(err);
-      return res.status(500).json({success: false, data: err});
-    }
-    // SQL Query > Select Data
-    const query = client.query('SELECT * FROM Item ORDER BY Item DESC limit 10;');
-    // Stream results back one row at a time
-    query.on('row', (row) => {
-      results.push(row);
-    });
-    // After all data is returned, close connection and return results
-    query.on('end', () => {
-      done();
-      return res.json(results);
-    });
-  });
-
 });
 
-const users = []
 // Should add an item to the database.
 app.post('/items/:name', (req, res) => {
-  const name = req.body
-  const time = Date.now()
-  
-    database.connect(conString, function (err, client, done) {
-      if (err) {
-        // pass the error to the express error handler
-        return next(err)
-      }
-      client.query('INSERT INTO Item (name, age) VALUES ($1, $2);', [name.name, time], function (err, result) {
-        done() //this done callback signals the pg driver that the connection can be closed or returned to the connection pool
-  
-        if (err) {
-          // pass the error to the express error handler
-          return next(err)
-        }
-  
-        res.send(200)
-      })
-    })
+  var name = req.params.name;
+  var date = Date.now();
+  var onInsert = "I don't know what this is";
+
+  database.insert(name, date, onInsert);
 });
 
 //app.listen(3000);
